383621b91d38a0340c52b8cf338a9f76f8d3c632
EmmHale <35249069+EmmHale@users.noreply.github.com>
2021-08-22 22:13:29 -0700

    Documentation update
    Documentation update

--- Script/MeshCombiner.cs
+++ Script/MeshCombiner.cs
@@ -1,4 +1,9 @@
-﻿using System.Collections;
+﻿/**********************************
+ *Author: Emmett Hale 
+ * 
+ *Purpose: A simple mesh combiner that conserves material types
+ **********************************/
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
